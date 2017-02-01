dbf367876e3f9051c2a7b6dab7f8b8dd1dca3bfa
jimmybengtsson <jb223pu@student.lnu.se>
2017-02-01 17:43:44 +0100

    Added price

--- app/dao/Book.js
+++ app/dao/Book.js
@@ -1,12 +1,13 @@
 'use strict';
 
-function Book(id, title, author, genre, publishDate, description) {
+function Book(id, title, author, genre, publishDate, description, price) {
     this.id = id;
     this.title = title;
     this.author = author;
     this.genre = genre;
     this.publishDate = publishDate;
     this.description = description;
+    this.price = price
 }
 
 module.exports = Book;
--- app/resources/GetBooksResource.js
+++ app/resources/GetBooksResource.js
@@ -9,9 +9,9 @@
 
         let list = [];
 
-        let bookOne = new Book('1', 'Title', 'Author', 'Genre', 'PublishDate', 'Description');
-        let bookTwo = new Book('2', 'JavaScript: The Good Parts', 'Douglas Crockford', '-', '2008-05-01', '-');
-        let bookThree = new Book('3', 'Learning Web Design', 'Jennifer Niederst Robbins', 'Learning', '2012-08-01', '4th edition');
+        let bookOne = new Book('1', 'Title', 'Author', 'Genre', 'PublishDate', 'Description', '1');
+        let bookTwo = new Book('2', 'JavaScript: The Good Parts', 'Douglas Crockford', '-', '2008-05-01', '-', '1');
+        let bookThree = new Book('3', 'Learning Web Design', 'Jennifer Niederst Robbins', 'Learning', '2012-08-01', '4th edition', '1');
 
         list.push(bookOne, bookTwo, bookThree);
 
