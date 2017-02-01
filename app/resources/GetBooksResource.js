93500c56ea5138ac0c55e1fa5277957b2bb320fd
jimmybengtsson <jb223pu@student.lnu.se>
2017-02-01 17:40:16 +0100

    Completed Assignment 1.

--- app/resources/GetBooksResource.js
+++ app/resources/GetBooksResource.js
@@ -1,10 +1,21 @@
 (function () {
     "use strict";
 
-    var LibraryDAO = require('../dao/LibraryDAO');
+    let LibraryDAO = require('../dao/LibraryDAO');
+    let Book = require('../dao/Book.js');
 
 
     module.exports = function (callback, title) { // The title is optional and is only present when searching. (You need yo modify the books.js file first)
+
+        let list = [];
+
+        let bookOne = new Book('1', 'Title', 'Author', 'Genre', 'PublishDate', 'Description');
+        let bookTwo = new Book('2', 'JavaScript: The Good Parts', 'Douglas Crockford', '-', '2008-05-01', '-');
+        let bookThree = new Book('3', 'Learning Web Design', 'Jennifer Niederst Robbins', 'Learning', '2012-08-01', '4th edition');
+
+        list.push(bookOne, bookTwo, bookThree);
+
+        callback(JSON.stringify(list));
 
     };
 
